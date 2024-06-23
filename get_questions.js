685dad4ac46b69db26c7ee808722f1f868889282
Veronica Nutting <veronicanutting@VeronicasNewAir.localdomain>
2024-06-22 20:33:32 -0700

    adding question generation

---
+++ get_questions.js
@@ -0,0 +1,54 @@
+import OpenAI from "openai";
+import fs from "fs";
+
+const step_1 = `You will be given a set of doctor's notes. 
+Summarize the actions the patient should be taking in no more than 4 sentences.
+Here is the note: `;
+
+const step_2 = `Given a summary of doctor's notes about the actions their patient 
+should take before their next visit, come up with a list of questions to ask the patient 
+to see how they have been doing. Only return the questions. Here is the summary: `;
+
+const step_3 = `Given a list of questions to ask a doctor's patient 
+to see how they have been doing since their last visit, order the questions by importance.
+The most important questions are those that relate to medication changes, lab orders that 
+should have been completed, and referrals. Return the three most important questions. Add one
+more question asking if there have been any significant health updates since the last visit. 
+Return only the questions as a list of strings: `;
+
+const openAIClient = new OpenAI();
+
+export async function get_questions(verbose=false) {
+    const doctors_note_file = "doc_notes/doc_note_1.text";
+    const doctors_note = fs.readFileSync(doctors_note_file, "utf8");
+    const notes_summary = await getAIResponse(step_1+doctors_note);
+    const questions_list = await getAIResponse(step_2+notes_summary);
+    const questions_top = await getAIResponse(step_3+questions_list);
+
+    if (verbose) {
+        console.log("Step 1: ", step_1)
+        console.log("Response: ", notes_summary, "\n");
+        console.log("Step 2: ", step_2)
+        console.log("Response: ", questions_list, "\n");
+        console.log("Step 3: ", step_3)
+        console.log("Response: ", questions_top, "\n");
+    }
+
+    return questions_top;
+}
+
+async function getAIResponse(prompt) {
+    const response = await openAIClient.chat.completions.create({
+        messages: [{ role: "system", content: "You are a hospital administrator" },
+                { role: "user", content: prompt }
+        ],
+        model: "gpt-3.5-turbo",
+    });
+    return response.choices[0].message.content;
+}
+
+async function main() {
+    await get_questions(true);
+}
+
+main();
